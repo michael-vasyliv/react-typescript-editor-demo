b1fbd22c26802d80d9686e3465a6599a1b76e973
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2021-08-20 16:08:55 +0300

    feat: added ckeditor

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,21 +1,11 @@
+import { CkEditor } from '@components/ckeditor';
 import React from 'react';
 import './app.scss';
-import Logo from './logo.svg';
-
-const { NODE_ENV = '' } = process.env;
 
 export function App() {
     return (
         <main className="main">
-            <Logo className="app-logo" />
-            {NODE_ENV}
-            <a
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
+            <CkEditor />
         </main>
     );
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-duplicates */
 declare module '*.svg' {
     import * as React from 'react';
 
@@ -9,3 +10,25 @@ declare module '*.svg' {
 type ExtendProps<T extends (...args: any) => any> = T extends (first: infer P) => any ? P : never;
 
 type Nullable<T> = T | undefined;
+
+declare module '@ckeditor/ckeditor5-react' {
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+    import { EditorConfig } from '@ckeditor/ckeditor5-core/src/editor/editorconfig';
+    import * as React from 'react';
+
+    const Event: any;
+
+    const CKEditor: React.FunctionComponent<{
+        disabled?: boolean;
+        editor: typeof ClassicEditor;
+        data?: string;
+        id?: string;
+        config?: EditorConfig;
+        onReady?: (editor: ClassicEditor) => void;
+        onChange?: (event: Event, editor: ClassicEditor) => void;
+        onBlur?: (event: Event, editor: ClassicEditor) => void;
+        onFocus?: (event: Event, editor: ClassicEditor) => void;
+        onError?: (event: Event, editor: ClassicEditor) => void;
+    }>;
+    export { CKEditor };
+}
